ba4698014075eac5bca27ca0147ee505c25e6386
Malpercio <root@malpercio.dev>
2024-12-02 17:33:52 -0600

    chore: clean up old comments

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,3 @@
-// use actix_web::{get, web, HttpServer, Responder};
-// use color_eyre::eyre::Result;
-// use lazy_static::lazy_static;
-// use serde::{Deserialize, Serialize};
-// use tera::Tera;
-
-// #[get("/forums")]
-// async fn forums_handler(
-//     _action: actix_web::web::Path<(String, String)>,
-//     _state: actix_web::web::Data<AppState>,
-// ) -> impl Responder {
-//     let ctx = tera::Context::new();
-
-//     let rendered = TEMPLATES.render("components/forums.html", &ctx).unwrap();
-
-//     actix_web::HttpResponse::Ok().body(rendered)
-// }
-
-// }
-
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
     let router = atbb::run().await?;
